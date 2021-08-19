a8be90159518d2a28b3c7986730ae1ecbfbbc663
bgl-di <88548052+bgl-di@users.noreply.github.com>
2021-08-19 20:51:14 +0300

    language, variables and functions names, exit function

--- src/ice_cream.cpp
+++ src/ice_cream.cpp
@@ -8,58 +8,50 @@ const set<short, greater<short>> coins_nominal{ 5, 10, 15 };
 map<short, short> moneybox;
 const short price = 5;
 
-short money_to_change(short money) {
-	short x;
-	x = money - price;
-	return x;
-}
-
-
 void money_to_coins(short money) {
-	int k;
+	int number_of_coins;
 	for (const int &i : coins_nominal) {
-		k = money / i;
-		moneybox[i] += k;
-		money -= k * i;
+		number_of_coins = money / i;
+		moneybox[i] += number_of_coins;
+		money -= number_of_coins * i;
 	}
 }
 
-
 void change_to_coins(short change) {
-	short k = 0;
+	short number_of_coins = 0;
 	short y = change;
 
 	for (const int &i : coins_nominal) {
 		if (y >= i) {
-			k = change / i;
-			y -= (min(moneybox[i], k) * i);
+			number_of_coins = change / i;
+			y -= (min(moneybox[i], number_of_coins) * i);
 		} else
 			continue;
 	}
 
 	if (y > 0) {
-		short mon = change + price;
-		cout << "Ќедостаточно денег в автомате, вы получите: " << mon << endl;
+		short money = change + price;
+		cout << "Not enough coins in the machine: " << money << endl;
 		for (const int &i : coins_nominal) {
-			k = mon / i;
-			mon -= min(moneybox[i], k) * i;
-			moneybox[i] -= min(moneybox[i], k);
+			number_of_coins = money / i;
+			money -= min(moneybox[i], number_of_coins) * i;
+			moneybox[i] -= min(moneybox[i], number_of_coins);
 		}
 		return;
 	}
 
-	cout << "¬ы получите мороженое и сдачу в размере: " << change << " рублей"
+	cout << "You will get an ice cream and " << change << " dollars change"
 			<< endl;
 	for (const int &i : coins_nominal) {
-		k = change / i;
-		change -= min(moneybox[i], k) * i;
-		cout << i << "-рублевых: " << min(moneybox[i], k) << endl;
-		moneybox[i] -= min(moneybox[i], k);
+		number_of_coins = change / i;
+		change -= min(moneybox[i], number_of_coins) * i;
+		cout << i << "-dollar: " << min(moneybox[i], number_of_coins) << endl;
+		moneybox[i] -= min(moneybox[i], number_of_coins);
 	}
 }
 
 
-void empty_moneybox() {
+void moneybox_initialization() {
 	for (const int &i : coins_nominal) {
 		moneybox[i] = 0;
 	}
@@ -68,10 +60,9 @@ void empty_moneybox() {
 
 short getvalue() {
 	short money;
-	while (!(cin >> money) || (cin.peek() != '\n') || money < 5) {
+	while (!(cin >> money) || (cin.peek() != '\n') || (money < 5 && money != 0)) {
 		cin.clear();
-		while (cin.get() != '\n')
-			;
+		while (cin.get() != '\n');
 		cout << "Input error!" << endl;
 	}
 	return money;
@@ -81,13 +72,16 @@ short getvalue() {
 int main() {
 	short change;
 	short money;
-	empty_moneybox();
+	moneybox_initialization();
 
-	for (int i = 0; i <= 1000; i++) {
+	while (true) {
+		cout << "Enter the amount of money, to exit enter zero:" << endl;
 		money = getvalue();
+		if (money == 0)
+			return 0;
 		money_to_coins(money);
 
-		change = money_to_change(money);
+		change = money - price;
 		change_to_coins(change);
 		for (const int &i : coins_nominal) {
 			cout << i << " - " << moneybox[i] << endl;
